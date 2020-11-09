a2578d37588760dcf41415c6ab22622f3e60e25a
Eric Amrhein <eamrhein@protonmail.ch>
2020-11-09 10:00:15 -0800

    google analytics

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,9 +3,11 @@
   import Projects from '../components/Projects.svelte'
   import About from '../components/About.svelte'
   import Contactbar from '../components/Contactbar.svelte'
+  import { GoogleAnalytics } from '@beyonk/svelte-google-analytics'
 </script>
 
 
+<GoogleAnalytics properties={[ 'google property id A', ...'google property id X' ]} />
 <svelte:head>
   <title>Eric Amrhein - Web Developer</title>
 </svelte:head>
